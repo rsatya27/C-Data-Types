0c1936a96f8c7fabb29123cc6b1364eaac650467
rsatya27 <rajneesh.satya@mcvts.org>
2024-09-10 09:20:53 -0400

    Add files via upload

---
+++ zinga.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+int main()
+{
+    int a, b;
+    float c, d;
+    a = 10;
+    b = 5;
+    c = 2.5;
+    d = 1.5;
+    string names1 = "Hello World";
+    char rizz = 'a';
+    bool sigma = false;
+
+    if[sigma]
+    cout << a * b << endl;
+    cout << c + d << endl;
+    
+}
